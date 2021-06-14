14d83b6ce0a947e7b88e6338556ebd39e812147e
edoomm <edoomm8@gmail.com>
2021-06-14 15:09:31 -0500

    se empieza con la función first

--- src/LL1.py
+++ src/LL1.py
@@ -75,35 +75,6 @@ if gx2.inieval():
     print("No terminales:", anll1.Vn, "\nTerminales:", anll1.Vt)
     lista = anll1.listaReglas[0][3:len(anll1.listaReglas)]
     print("Lista:", lista)
-    # print("First de lista:", anll1.first(lista))
+    print("First de lista:", anll1.first(lista))
 else:
     print("INCORRECTO")
-
-simb = AFN()
-simb.crearAFNBasico('a', 'z')
-simb.unir(AFN(simbinf = "A", simbsup = "Z"))
-# simb.concatenar(AFN(simbinf = 'a', simbsup = 'z').unir(AFN(simbinf='A', simbsup='Z')))
-s2 = AFN(simbinf='a', simbsup='z')
-s2.unir(AFN(simbinf='A', simbsup='Z')).unir(AFN(simbinf='0', simbsup='9')).unir(AFN(simbinf='_')).unir(AFN(simbinf="'"))
-s2.cerradurak()
-simb.concatenar(s2)
-simb.unir(AFN(simbinf='♣'))
-
-pc = AFN(simbinf=';')
-
-flecha = AFN(simbinf='-')
-flecha.concatenar(AFN(simbinf='>'))
-
-orafn = AFN(simbinf="|") # No funciona con \|
-
-esp = AFN(simbinf=' ')
-esp.cerradurap()
-
-# afd = AFD(simb)
-# afd.exportarAFD("test")
-# file = ""
-# analizador = AnalizadorLexico("test" if file == "" else file)
-analizador = AnalizadorLexico()
-analizador.union([simb, pc, flecha, orafn, esp])
-afd = AFD(analizador.afn)
-afd.exportarAFD("test")
