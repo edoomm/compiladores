215946d750465bd2e0489bfe986b7e573a4347d7
edoomm <edoomm8@gmail.com>
2021-06-15 23:36:58 -0500

    construcción de tabla LL(1)

--- src/LL1.py
+++ src/LL1.py
@@ -6,6 +6,10 @@ class LL1(object):
         self.Vn             = set()
         self.Vt             = set()
         self.listaReglas    = [] if gramatica == None else gramatica._ListaReglas
+        self.tokens         = {}
+        self.tabla          = []
+        self.cabcol         = []
+        self.cabfil         = []
 
     ''' Métodos
     '''
@@ -19,31 +23,10 @@ class LL1(object):
             i=0
         for fila in self.listaReglas:
             for dato in fila:
-                if not dato in self.Vn:
+                if not dato in self.Vn and dato != "EPSILON":
                     self.Vt.add(dato)
 
-    def first(self, lista: list) -> set:
-        """Se encuentra el primer terminal de una lista que describe una regla
-
-        Args:
-            lista (list): Lista que puede contener terminales y no terminales
-
-        Returns:
-            set: El conjunto de tokens que se calcularon del first
-        """
-        conjunto = set()
-        if lista == None or len(lista) == 0:
-            return None
-        if lista[0] in self.Vt:
-            conjunto.add(lista[0])
-        else:
-            for elem in self.listaReglas:
-                if elem[0] == lista[0]:
-                    conjunto += self.first(elem[1:len(elem)])
-                    # Acá pienso que se debe descartar toda derivación que sea A -> A, porque si no, se hace recursivo el pedo xd
-        return conjunto
-    
-    def first2(self,lista :list,simbolos :set):
+    def first(self,lista :list,simbolos :set):
         bandera=1   
         lexema=""
         if lista[0] in self.Vt:
@@ -65,7 +48,7 @@ class LL1(object):
                else:
                     for f in self.listaReglas:
                         if f[0]==lexema:
-                            self.first2(f,simbolos)
+                            self.first(f,simbolos)
                         #return simbolos  
             bandera=1
         return simbolos    
@@ -79,7 +62,7 @@ class LL1(object):
                     if i!=0:
                         if lista[0]==fila[i] and i<len(fila)-1:
                             listAux.append(fila[i+1])
-                            self.first2(listAux,conjunto)
+                            self.first(listAux,conjunto)
                             listAux=[]
         else:
             for fila in self.listaReglas:
@@ -90,7 +73,7 @@ class LL1(object):
                             
                             listAux.append(fila[i+1])
                             
-                            self.first2(listAux,conjunto)
+                            self.first(listAux,conjunto)
                             listAux=[]
                             listAux.append(fila[0])
                             
@@ -103,51 +86,72 @@ class LL1(object):
             conjunto.remove("EPSILON")
         
         return conjunto
-
-    """
-    
-    
-    
-    def first2(self,lista :list,simbolos :set):
-        bandera=1   
-        lexema=""
-        for fila in self.listaReglas:
-            for i in range(len(fila)):
-                if len(fila) == len(lista):
-                    if fila[i] != lista[i]:
-                        bandera=0
-                else:
-                    bandera=0    
-            if bandera==1:
-               
-               lexema=fila[1]
-               print(lexema)
-               if lexema in self.Vt:
-                    simbolos.add(lexema)     
-                    return simbolos
-               else:
-                    for f in self.listaReglas:
-                        if f[0]==lexema:
-                            self.first2(f,simbolos)
-                            
-                    return simbolos 
-            bandera=1
-    """
-
-                        
-
-    """
-    def follow(self, terminal: str) -> set:
-        Calcula el follow de un símbolo terminal
+ 
+    def asignarTokens(self, esexpnum = False):
+        """Asigna los tokens de la gramática de gramáticas con la gramática de entrada
 
         Args:
-            terminal (str): El símbolo terminal
+            esexpnum (bool, optional): Sirve para asignar automaticamente los tokens de enum.txt. Defaults to False.
+        """
+        if esexpnum == False:
+            for terminal in self.Vt:
+                self.tokens[terminal] = input("Ingrese el token para '" + terminal + "': ")
+        else:
+            self.tokens = {'ENTRE': '40', 'MENOS': '20', 'MAS': '10', 'P_D': '60', 'NUM': '70', 'P_I': '50', 'POR': '30'}
+        print(self.tokens)
 
-        Returns:
-            set: Regresa el conjunto de tokens que se obtuvieron
-        
-        pass
-    """    
+    def construirTabla(self):
+        """Construye la tabla de análisis sintáctico LL(1)
+        """
+        self.cabcol = list(self.tokens.values())
+        self.cabcol.append("$")
+
+        self.cabfil = list(self.Vn) + self.cabcol
+
+        colaux = [''] + self.cabcol
+        self.tabla.append(colaux)
+        flg = 0
+        for fila in self.cabfil:
+            f = [fila] + [-1 for i in range(0, len(self.cabcol))]
+            if fila == self.cabcol[flg] and fila != "$":
+                f[flg + 1] = "pop"
+                flg += 1
+            elif fila == "$":
+                f[flg + 1] = "acep"
+                flg += 1
+            self.tabla.append(f)
+
+        self.analizarReglas()
+
+    def analizarReglas(self):
+        """Analiza las reglas una por una para construir la tabla de análisis sintáctico LL1
+        """
+        num = 1
+        for regla in self.listaReglas:
+            lst = regla[1: len(regla)]
+            cols = self.first(lst, set())
+            if len(cols) == 0: # Significa que fue EPSILON el first
+                cols = self.follow([regla[0]], set())
+            for col in cols:
+                fila = self.cabfil.index(regla[0]) + 1
+                if col != "$":
+                    columna = self.cabcol.index(self.tokens[col]) + 1
+                else:
+                    columna = len(self.cabcol)
+                self.tabla[fila][columna] = [lst, num]
+            num += 1
+      
+    def imprimirTabla(self):
+        """Imprime la tabla de análisis sintáctico LL(1)
+        """
+        print(end='\t')
+        for cabecera in list(self.tokens.keys()):
+            print(cabecera, end='\t')
+        print("")
+        for fila in self.tabla:
+            for celda in fila:
+                print(celda, end='\t')
+            print("")
 
 #################################################################################################
 #   TEST SECTION                                                                                #
@@ -155,25 +159,19 @@ class LL1(object):
 
 analizador = AnalizadorLexico("grams")
 op = 12
-#cadena = "E->E MAS T|E MENOS T|T;T->T POR F|T ENTRE F|F;F->P_I E P_D|NUM;"
-cadena = "E->T Ep;Ep->MAS T Ep|MENOS T Ep|EPSILON;T->F Tp;Tp->POR F Tp;Tp->ENTRE F Tp|EPSILON;F->P_I E P_D|NUM;"
+cadena = "E->T E';E'->MAS T E'|MENOS T E'|EPSILON;T->F T';T'->POR F T';T'->ENTRE F T'|EPSILON;F->P_I E P_D|NUM;"
 analizador.CadenaSigma = input("\nCadena a analizar: ") if op == 1 else cadena
 if op != 1:
     print("\nCadena a analizar:", cadena, "\n")
 
 gx2 = GramaticasDeGramaticas(analizador)
 if gx2.inieval():
-    #gx2.imprimirListaReglas()
-    
     anll1 = LL1(gx2)
     anll1.llenarVnAndVt()
-    print("No terminales:", anll1.Vn, "\nTerminales:", anll1.Vt)
-    #lista = anll1.listaReglas[0][3:len(anll1.listaReglas)]
-    lista=["T"]
-    print("Lista:", lista)
-    con=set()
-    anll1.follow(lista,con)
-    
-    print("Follow de lista:", con)
+
+    anll1.asignarTokens(esexpnum=True)
+    anll1.construirTabla()
+
+    anll1.imprimirTabla()
 else:
     print("INCORRECTO")
